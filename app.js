94c6bcb73ee67d1251200c017523f98395486193
smercier <seb@h.com>
2017-12-08 10:50:43 +0100

    5

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ angular.module('LunchCheck',[])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
 
+	$scope.CheckingItems = function(){
+		 //var separator = ",";
+		 var stringToSplit = $scope.name;
+		 if (stringToSplit == "")
+		 	console.log("Please enter Data first");
+		
+		else if (stringToSplit != "")
+			var arrayOfStrings = stringToSplit.split(",");
+			var x = arrayOfStrings.length;
+		
+
+	        if (x <= 3)
+	        	console.log("Enjoy");
+	        else if (x > 3)
+	        	console.log("Too much");
+
+	};
+
 }
 
 } ());

b5b033ff1b6198519be168d51c7b2162ee138f22
smercier <seb@h.com>
2017-12-08 15:26:52 +0100

    Final Version

--- app.js
+++ app.js
@@ -8,21 +8,22 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope){
 
 	$scope.CheckingItems = function(){
-		 //var separator = ",";
 		 var stringToSplit = $scope.name;
-		 if (stringToSplit == "")
-		 	console.log("Please enter Data first");
-		
-		else if (stringToSplit != "")
-			var arrayOfStrings = stringToSplit.split(",");
+		 var y = typeof stringToSplit; 
+		 
+		 if (y === 'undefined'){
+		 	return $scope.result = ("Please enter Data first");
+		 }
+		else if (y === 'string'){
+			var arrayOfStrings = stringToSplit.split(",");	
 			var x = arrayOfStrings.length;
-		
-
-	        if (x <= 3)
-	        	console.log("Enjoy");
-	        else if (x > 3)
-	        	console.log("Too much");
-
+			if (x <= 3){
+	        	return $scope.result=("Enjoy");
+			}
+	        else if (x > 3){
+	        	return $scope.result=("Too much");
+	        }
+		}
 	};
 
 }
